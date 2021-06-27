3a2a48a1115922746bbaa883bc301614a034658a
Juan Delgado <roloenusa@msn.com>
2021-06-26 22:19:45 -0700

    Add editable filelds to moment

--- src/containers/Moment.js
+++ src/containers/Moment.js
@@ -9,18 +9,64 @@ import Main from '@components/Main';
 import { http } from '@services/Backend';
 import TagLink, { Tags }from '@components/TagLink';
 
+const IconRow = styled.div`
+  display: flex;
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+`;
+
+const IconBtn = styled.i`
+  cursor: pointer;
+
+  width: 3rem;
+  height: 3rem;
+  font-size: 2rem;
+  border-radius: 50%;
+  border-width: 0px;
+  border-style: solid;
+  border-color: var(--bg-accent);
+  padding: 5px;
+  margin: 2px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: filter 300ms;
+  background-color: var(--bg-primary);
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
+
+  ${ ({primary}) => primary && `--color: var(--darkteal);` }
+  ${ ({danger}) => danger && `--color: var(--brightfucsia);` }
+
+  ${
+    ({primary, danger}) => {
+      if (primary || danger) {
+        return `&:hover {
+          background-color: var(--color);
+          color: var(--bg);
+        }`;
+      }
+    }
+  }
+`;
+
 const Moment = () => {
   const { id } = useParams();
 
   const [moment, setMoment] = useState({});
+  const [title, setTitle] = useState();
+  const [text, setText] = useState();
   const [assets, setAssets] = useState([]);
   const [pickImg, setPickImg] = useState(false);
   const [showcase, setShowcase] = useState(false);
+  const [edit, setEdit] = useState(false);
 
   useEffect(async () => {
     const response = await http.get(`/moments/${id}`);
 
     setMoment(response.data);
+    setTitle(response.data.title);
+    setText(response.data.text);
     setAssets(response.data.assets || []);
   }, []);
 
@@ -43,12 +89,47 @@ const Moment = () => {
     return undefined;
   }
 
+  const isEditing = () => {
+    if (edit) {
+      return (
+        <IconRow>
+          <IconBtn className="la la-save" onClick={() => setEdit(false)} primary/>
+          <IconBtn className="la la-times" onClick={() => setEdit(false)} danger/>
+        </IconRow>
+      )
+    }
+    return (
+      <IconRow>
+        <IconBtn className="la la-edit" onClick={() => setEdit(true)} />
+      </IconRow>
+    );
+  }
+
   return (
     <Main>
+      { isEditing() }
       <HeroBox>
-        <Title>{moment.title}</Title>
+        <Title
+          contentEditable={edit}
+          role="textbox"
+          style={{maxWidht: '100%'}}
+          onBlur={(e) => setTitle(e.target.innerText)}
+          suppressContentEditableWarning={true}
+        >
+          {title}
+        </Title>
+
         <Hr />
-        <Subtitle text>{moment.text}</Subtitle>
+        <Subtitle
+          text
+          contentEditable={edit}
+          role="textbox"
+          style={{maxWidht: '100%'}}
+          onBlur={(e) => setText(e.target.innerText)}
+          suppressContentEditableWarning={true}
+        >
+          {text}
+        </Subtitle>
 
         <Tags>{moment.tags && moment.tags.map((tag, i) => <TagLink key={`t-${i}`} tag={tag.name} />)}</Tags>
       </HeroBox>
